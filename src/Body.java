ea9cfc7f58c40096bd3a6c563c04ec25738a2dd8
Gangster Toboggan <chicken@nope.com>
2022-12-29 16:20:38 -0400

    Added getter/setters for planet and shit

--- src/Body.java
+++ src/Body.java
@@ -2,21 +2,21 @@ public interface Body {
 
     public Vec2 getPos();
 
-    public Vec2 setPos(Vec2 position);
+    public void setPos(Vec2 position);
 
     public Vec2 getVel();
 
-    public Vec2 setVel(Vec2 velocity);
+    public void setVel(Vec2 velocity);
 
     public double getAng();
 
-    public double setAng(double angle);
+    public void setAng(double angle);
 
     public double getAngVel();
 
-    public double setAngVel(double angularVelocity);
+    public void setAngVel(double angularVelocity);
 
     public double getMass();
 
-    public double setMass(double mass);
+    public void setMass(double mass);
 }
--- src/Planet.java
+++ src/Planet.java
@@ -1,3 +1,57 @@
 public class Planet implements Body {
+    // fields
 
+    private Vec2 pos;
+
+    private Vec2 vel;
+
+    private double ang;
+
+    private double angVel;
+
+    private double mass;
+
+    // constructor
+    // non-static methods
+    public Vec2 getPos() {
+        return this.pos;
+    }
+
+    public void setPos(Vec2 position) {
+        this.pos = position;
+    }
+
+    public Vec2 getVel() {
+        return this.vel;
+    }
+
+    public void setVel(Vec2 velocity) {
+        this.vel = velocity;
+    }
+
+    public double getAng() {
+        return this.ang;
+    }
+
+    public void setAng(double angle) {
+        this.ang = angle;
+    }
+
+    public double getAngVel() {
+        return this.angVel;
+    }
+
+    public void setAngVel(double angularVelocity) {
+        this.angVel = angularVelocity;
+    }
+
+    public double getMass() {
+        return this.mass;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    // static methods
 }
