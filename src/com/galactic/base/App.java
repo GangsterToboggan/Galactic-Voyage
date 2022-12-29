835ade97076cf8ba8f56cf5d58b9c69cc1e961fe
Gangster Toboggan <chicken@nope.com>
2022-12-29 17:55:27 -0400

    Defined the moon and earth position, velocities, and masses at start

--- src/com/galactic/base/App.java
+++ src/com/galactic/base/App.java
@@ -1,5 +1,7 @@
 package com.galactic.base;
 
+import java.math.BigDecimal;
+
 import com.galactic.math.Vec2;
 import com.galactic.ship.Ship;
 import com.galactic.spaceobjects.Planet;
@@ -10,6 +12,7 @@ public class App {
      * Units
      * Distance: meters
      * Time: Seconds
+     * Mass: Kg
      * Velocity: Meters / Second
      * Angles: Radians (defined like the unit circle, x axis is 0 and positive
      * direction is counter clock-wise)
@@ -20,7 +23,9 @@ public class App {
 
     Planet earth = new Planet(new Vec2(0, 0), new Vec2(0, 0), 0, 2 * Math.PI / 86400, 5.972 * Math.pow(10, 24));
 
-    Planet moon = new Planet(null, null, 0, 0, 0);
+    Planet moon = new Planet(new Vec2(new BigDecimal(0.3633 * Math.pow(10, 9)), new BigDecimal(0)),
+            new Vec2(new BigDecimal(0), new BigDecimal(1082)), 0, 2 * Math.PI / (27.3217 * 86400),
+            0.07346 * Math.pow(10, 24));
 
     Ship ship = new Ship(null, null, 0, 0, 0);
 
