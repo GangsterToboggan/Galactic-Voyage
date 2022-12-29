487c114dd2563241ebeaad8569e883d204cf8bb6
Gangster Toboggan <chicken@nope.com>
2022-12-29 15:55:50 -0400

    Added some abstract methods to body

--- src/Body.java
+++ src/Body.java
@@ -1,5 +1,22 @@
 public interface Body {
-    // fields
 
-    // methods
+    public Vec2 getPos();
+
+    public Vec2 setPos(Vec2 position);
+
+    public Vec2 getVel();
+
+    public Vec2 setVel(Vec2 velocity);
+
+    public double getAng();
+
+    public double setAng(double angle);
+
+    public double getAngVel();
+
+    public double setAngVel(double angularVelocity);
+
+    public double getMass();
+
+    public double setMass(double mass);
 }
